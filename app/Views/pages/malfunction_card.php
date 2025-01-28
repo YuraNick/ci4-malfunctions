f490a67fe36ae9fae6c1666bb0bfc6b00a3c64a0
Yuriy <505-565@mail.ru>
2025-01-29 00:05:35 +0500

    malfunction card added and siple formatted

--- app/Views/pages/malfunction_card.php
+++ app/Views/pages/malfunction_card.php
@@ -16,4 +16,78 @@
   </div>
 </form>
 
-<pre><?= print_r($data, true) ?></pre>  
+<h4>Неисправность:</h4>
+<?php foreach($data['malfunction'] as $malfunction): ?>
+  <table>
+    <?php foreach($malfunction as $key => $val): ?>
+      <tr>
+        <td><?=$key?></td>
+        <td><?=$val?></td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+<?php endforeach; ?>
+<br>
+<h4>Статусы и комментарии диспетчеров:</h4>
+<?php foreach($data['dispatcher_confirms'] as $dispatcher_confirm): ?>
+  <table>
+    <?php foreach($dispatcher_confirm as $key => $val): ?>
+      <tr>
+        <td><?=$key?></td>
+        <td><?=$val?></td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+<?php endforeach; ?>
+<br>
+<h4>Вопросы диспетчеров в техподдержку:</h4>
+<?php foreach($data['dispatcher_support_questions'] as $row): ?>
+  <table>
+    <?php foreach($row as $key => $val): ?>
+      <tr>
+        <td><?=$key?></td>
+        <td><?=$val?></td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+<?php endforeach; ?>
+<br>
+<h4>Ответы техподдержки на вопросы диспетчеров:</h4>
+<?php foreach($data['dispatcher_support_answers'] as $row): ?>
+  <table>
+    <?php foreach($row as $key => $val): ?>
+      <tr>
+        <td><?=$key?></td>
+        <td><?=$val?></td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+<?php endforeach; ?>
+<br>
+<h4>Вопросы техподдержки разработчикам:</h4>
+<?php foreach($data['support_developer_questions'] as $row): ?>
+  <table>
+    <?php foreach($row as $key => $val): ?>
+      <tr>
+        <td><?=$key?></td>
+        <td><?=$val?></td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+<?php endforeach; ?>
+<br>
+<h4>Ответы разработчиков техподдержке:</h4>
+<?php foreach($data['support_developer_answers'] as $row): ?>
+  <table>
+    <?php foreach($row as $key => $val): ?>
+      <tr>
+        <td><?=$key?></td>
+        <td><?=$val?></td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+<?php endforeach; ?>
+
+
+
+<!-- <pre><?= print_r($data, true) ?></pre>   -->
